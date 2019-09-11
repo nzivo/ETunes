ac318e5be0def6787765a8d8f9c6c8f4b3b88067
nzivo <nzivojohn@students.must.ac.ke>
2019-09-11 12:53:24 +0300

    Version 1.0.1 - Basic fetch music and play on click

---
+++ app/src/main/java/ke/co/batedesigns/etunes/Library/AllSongsActivity.java
@@ -0,0 +1,110 @@
+package ke.co.batedesigns.etunes.Library;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ke.co.batedesigns.etunes.Adapter.AudioListAdapter;
+import ke.co.batedesigns.etunes.Model.AudioModel;
+import ke.co.batedesigns.etunes.R;
+
+public class AllSongsActivity extends AppCompatActivity {
+
+    RecyclerView audioListView;
+    AudioListAdapter audioListAdapter;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_songs);
+
+        //Checks permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                return;
+            }
+        }
+
+        //Finds all the mp3 files in device and makes a list
+        getSongs();
+
+    }
+
+    public void getSongs(){
+        context = AllSongsActivity.this;
+        audioListView = findViewById(R.id.audioListView);
+
+        List<AudioModel> allAudioFiles = getAllAudioFromDevice(context);
+
+        audioListAdapter = new AudioListAdapter(context,allAudioFiles);
+        audioListView.setLayoutManager(new LinearLayoutManager(context));
+        audioListView.setAdapter(audioListAdapter);
+    }
+
+    public List<AudioModel> getAllAudioFromDevice(final Context context) {
+
+        final List<AudioModel> tempAudioList = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
+        Cursor c = context.getContentResolver().query(uri,
+                projection,
+                null,
+                null,
+                null);
+
+        if (c != null) {
+            while (c.moveToNext()) {
+
+                AudioModel audioModel = new AudioModel();
+
+                String path = c.getString(0);
+                String album = c.getString(1);
+                String artist = c.getString(2);
+
+                String name = path.substring(path.lastIndexOf("/") + 1);
+
+                audioModel.setaName(name);
+                audioModel.setaAlbum(album);
+                audioModel.setaArtist(artist);
+                audioModel.setaPath(path);
+
+                tempAudioList.add(audioModel);
+            }
+            c.close();
+        }
+
+        return tempAudioList;
+    }
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getSongs();
+                    Toast.makeText(context,"Permission granted",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context,"Permission denied",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/ke/co/batedesigns/etunes/MainActivity.java
+++ app/src/main/java/ke/co/batedesigns/etunes/MainActivity.java
@@ -1,6 +1,9 @@
 package ke.co.batedesigns.etunes;
 
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -9,10 +12,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import ke.co.batedesigns.etunes.Library.AllSongsActivity;
+import ke.co.batedesigns.etunes.Model.AudioModel;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    MediaPlayer mediaPlayer;
+    AudioModel audio;
+    SeekBar seekBar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_music:
-                    Intent allMusicIntent = new Intent(MainActivity.this, LibraryActivity.class);
+                    Intent allMusicIntent = new Intent(MainActivity.this, AllSongsActivity.class);
                     startActivity(allMusicIntent);
                     return true;
                 case R.id.navigation_equalizer:
@@ -48,6 +64,117 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Intent intent = getIntent();
+        audio = (AudioModel) intent.getSerializableExtra("audio");
+
+        TextView audioName = findViewById(R.id.audioName);
+        Button playBtn = findViewById(R.id.playBtn);
+        Button viewAllMediaBtn = findViewById(R.id.viewAllMedia);
+
+        if(audio!=null){
+            audioName.setText(audio.getaName());
+        }else{
+            audioName.setText(R.string.initializing);
+        }
+
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlaying();
+            }
+        });
+
+        viewAllMediaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                startActivity(new Intent(getApplicationContext(),AllSongsActivity.class));
+                //finish();
+            }
+        });
+
+    }//End of onCreate
+
+    public void startPlaying(){
+        if(mediaPlayer!=null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+
+        if(audio!=null){
+
+            Toast.makeText(getApplicationContext(),"Playing from device; "+audio.getaPath(),Toast.LENGTH_SHORT).show();
+            mediaPlayer = MediaPlayer.create(MainActivity.this, Uri.parse(audio.getaPath()));
+            mediaPlayer.start();
+
+        }else{
+
+            Toast.makeText(getApplicationContext(),"Playing from app",Toast.LENGTH_SHORT).show();
+            try {
+                AssetFileDescriptor afd = getAssets().openFd("piano.wav");
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                afd.close();
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        enableSeekBar();
+    }
+
+    public void stopPlaying(){
+        if(mediaPlayer!=null){
+            mediaPlayer.stop();
+        }
+    }
+
+    public void enableSeekBar(){
+        seekBar =  findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+                if(mediaPlayer!=null && mediaPlayer.isPlaying()){
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                }
+            }
+        }, 0, 10);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                // Update the progress depending on seek bar
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        stopPlaying();
+        super.onBackPressed();
     }
 
 }
